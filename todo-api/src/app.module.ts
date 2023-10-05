c9b12894cb57aeb44005bb28f2aefb88776553f0
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2023-10-04 23:16:17 -0300

    :sparkles: Added user resource

--- todo-api/src/app.module.ts
+++ todo-api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { TodoModule } from './todo/todo.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule, TodoModule],
+  imports: [PrismaModule, TodoModule, UserModule],
   controllers: [],
   providers: [],
 })
